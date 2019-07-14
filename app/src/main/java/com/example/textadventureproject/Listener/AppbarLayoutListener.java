c86d7901a77da5eaeb917da584bfbb50fab55a8d
pazer1 <elven404@gmail.com>
2019-07-14 12:24:31 +0900

    Merge branch 'master' of https://github.com/pazer1/TextAdventureProject
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/example/textadventureproject/Listener/AppbarLayoutListener.java
+++ app/src/main/java/com/example/textadventureproject/Listener/AppbarLayoutListener.java
@@ -1,4 +1,38 @@
 package com.example.textadventureproject.Listener;
 
-public class AppbarLayoutListener {
+import android.util.Log;
+
+import com.google.android.material.appbar.AppBarLayout;
+
+public abstract class AppbarLayoutListener implements AppBarLayout.OnOffsetChangedListener {
+
+    public enum State{
+        EXPANDED,
+        COLLAPSED,
+        IDEL
+    }
+
+    private State mCurrentState = State.IDEL;
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+
+        if(verticalOffset==0){
+            if(mCurrentState != State.EXPANDED){
+                onStateChanged(appBarLayout,State.EXPANDED);
+            }
+            mCurrentState = State.EXPANDED;
+        }else if(Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()){
+            if(mCurrentState != State.COLLAPSED){
+                onStateChanged(appBarLayout,State.COLLAPSED);
+            }
+            mCurrentState = State.COLLAPSED;
+        }else{
+            if(mCurrentState != State.IDEL){
+                onStateChanged(appBarLayout,State.IDEL);
+            }
+            mCurrentState = State.IDEL;
+        }
+    }
+    public abstract void onStateChanged(AppBarLayout appBarLayout, State state);
 }
--- app/src/main/java/com/example/textadventureproject/SplashActivity.java
+++ app/src/main/java/com/example/textadventureproject/SplashActivity.java
@@ -1,28 +1,35 @@
 package com.example.textadventureproject;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.widget.NestedScrollView;
 
-import android.content.Intent;
-import android.graphics.PixelFormat;
-import android.media.Image;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.textadventureproject.Listener.AppbarLayoutListener;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity  {
 
     Thread splashThread;
     ImageView splashText,mainSplash,backdrop;
-    CoordinatorLayout relativeLayout;
-
-
+    CoordinatorLayout coordinatorLayout;
+    Toolbar toolbar;
+    NestedScrollView nestedScrollView;
+    boolean isUp = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +37,41 @@ public class SplashActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
         backdrop = findViewById(R.id.backdrop);
+        coordinatorLayout = findViewById(R.id.main_content);
+        toolbar = findViewById(R.id.splash_toolbar);
+        nestedScrollView = findViewById(R.id.splash_nested);
+        nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                if(scrollY >=1000){
+                    CollapsingToolbarLayout.LayoutParams layoutParams = (CollapsingToolbarLayout.LayoutParams) toolbar.getLayoutParams();
+                    layoutParams.height = 1;
+                    toolbar.setLayoutParams(layoutParams);
+                    isUp = true;
+                }else if(isUp = true && scrollY<1000){
+                    CollapsingToolbarLayout.LayoutParams layoutParams = (CollapsingToolbarLayout.LayoutParams) toolbar.getLayoutParams();
+                    layoutParams.height = 100;
+                    toolbar.setLayoutParams(layoutParams);
+                }
+            }
+        });
+        AppBarLayout appBarLayout = findViewById(R.id.splash_appbar);
+        AppbarLayoutListener appbarLayoutListener = new AppbarLayoutListener() {
+            @Override
+            public void onStateChanged(AppBarLayout appBarLayout, State state) {
+                if(state.equals(State.EXPANDED)){
 
+                }else if(state.equals(State.COLLAPSED)){
+                    Log.d("STATE",state+"");
+
+                }
+            }
+        };
+        appBarLayout.addOnOffsetChangedListener(appbarLayoutListener);
         Glide.with(this).load(R.drawable.giphy).into(backdrop);
         StartAnimation();
-
     }
+
 
 
     //나는 너를 바다로 데려갈거야
